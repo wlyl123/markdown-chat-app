06e23b06b45da3056af856edc60d2cd2c503080d
miaoda <miaoda@baidu.com>
2026-04-27 22:17:36 +0800

    ISSUE: # Issue 配置了 GitHub Pages 部署所需的 base 路径、HashRouter 路由模式以及自动化的 GitHub Actions 工作流。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import { Toaster as ToastProvider } from "@/components/ui/toaster";
 
@@ -12,11 +12,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <ToastProvider />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
